a4e745a4341260d0f543fa0df74f01e338f556e8
Danny Yu <tiramisu@Dannys-MBP.home>
2022-09-10 05:03:56 -0400

    I like this layout better

--- JSscripts.js
+++ JSscripts.js
@@ -1,3 +1,6 @@
-('div').hover(function () {
-  (this).toggleClass('show-description');
-});
+function boooo() {
+  document.getElementById("btn__img").style.backgroundImage = "url(https://64.media.tumblr.com/tumblr_maiubutRWP1rfjowdo1_r1_500.gif)";
+}
+function mario() {
+  document.getElementById("btn__img").style.backgroundImage = "url(https://c.tenor.com/3uHxa8bk_lkAAAAi/mario-super-mario.gif)";
+}
