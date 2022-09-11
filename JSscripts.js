0452624d20d9c8ab2bc2e9b77cdf0ecd1a1ed946
Danny Yu <tiramisu@Dannys-MBP.home>
2022-09-11 02:59:50 -0400

    added more effects

--- JSscripts.js
+++ JSscripts.js
@@ -1,6 +1,21 @@
 function boooo() {
-  document.getElementById("btn__img").style.backgroundImage = "url(https://64.media.tumblr.com/tumblr_maiubutRWP1rfjowdo1_r1_500.gif)";
+  var but = document.getElementById("btn__img");
+  but.style.backgroundImage = "url(https://64.media.tumblr.com/tumblr_maiubutRWP1rfjowdo1_r1_500.gif)";
 }
 function mario() {
-  document.getElementById("btn__img").style.backgroundImage = "url(https://c.tenor.com/3uHxa8bk_lkAAAAi/mario-super-mario.gif)";
+  var but = document.getElementById("btn__img");
+  but.style.backgroundImage = "url(https://c.tenor.com/3uHxa8bk_lkAAAAi/mario-super-mario.gif)";
+}
+
+function booming() {
+  var boom = document.getElementsByTagName("link")[0];
+  var bext = document.getElementById("btn__boom");
+  if (boom.getAttribute("href") == "style.css") {
+    boom.setAttribute("href", "boom.css");
+    bext.textContent = "Turn back Time";
+  }
+  else {
+    boom.setAttribute("href", "style.css");
+    bext.textContent = "Stop it!";
+  }
 }
